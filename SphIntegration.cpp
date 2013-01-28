9178cd2ec05f43c0ad566d523c6be9eec2fb617d
Giovanni Rosotti <rosotti@usm.lmu.de>
2013-01-28 09:43:48 +0100

    Implemented timestep acceleration condition

--- SphIntegration.cpp
+++ SphIntegration.cpp
@@ -2,7 +2,7 @@
 // SphIntegration.cpp
 // ============================================================================
 
-
+#include <algorithm>
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
@@ -41,5 +41,17 @@ SphIntegration::~SphIntegration()
 // ============================================================================
 double SphIntegration::Timestep(SphParticle &part, EOS *eos)
 {
-  return (double) 0.1*part.h/(eos->SoundSpeed(part));
+  double timestep;
+  double amag;
+
+  //Courant condition
+  timestep = 0.1*part.h/(eos->SoundSpeed(part) + );
+
+  //Acceleration condition
+  amag = sqrt(part.a[0]*part.a[0] + part.a[1]*part.a[1] + part.a[2]*part.a[2]);
+  timestep = min(timestep, accel_mult*sqrt(part.h/(amag + small_number_dp));
+
+  //TODO: implement energy condition (once we will be solving the energy equation)
+
+  return timestep;
 }
