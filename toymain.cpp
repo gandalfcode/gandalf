e6636070e787ddaa2309db1a58ef06d608397b9a
Giovanni Rosotti <rosotti@usm.lmu.de>
2013-01-30 10:43:09 +0100

    Parameter file name is parsed from the command line

--- toymain.cpp
+++ toymain.cpp
@@ -1,11 +1,22 @@
-#include <cstdio>
+#include <iostream>
 #include "SphSimulation.h"
+using namespace std;
 
-int main(int argc, char* argv)
+int main(int argc, char** argv)
 {
   SphSimulation sim;
 
+  if (argc >= 2){
+    sim.paramfile = argv[1];
+  }
+  else {
+    cout << "No parameter file specified, aborting..." << endl;
+    exit(-1);
+  }
+
   sim.Setup();
+
+  sim.MainLoop();
 
   return 0;
 }
