5dfdf2fb81a3574875b26d44b9371a9ba3987235
Giovanni Rosotti <rosotti@usm.lmu.de>
2013-02-07 18:02:54 +0100

    Revert "Added inlining of kernel for testing"
    This reverts commit 9a364b2dd57094770e17cecca287398fa2938e64.

--- GradhSph.cpp
+++ GradhSph.cpp
@@ -108,11 +108,11 @@ int GradhSph::ComputeH(int i, int Nneib, SphParticle *neiblistpart, Parameters &
       for (k=0; k<ndim; k++) dv[k] = neiblistpart[jj].v[k] - parti.v[k];
       
       parti.div_v -= neiblistpart[jj].m*DotProduct(dv,dr,ndim)*
-	w1(drmag*parti.invh)*parti.hfactor*parti.invh;
+	kern->w1(drmag*parti.invh)*parti.hfactor*parti.invh;
       parti.rho += neiblistpart[jj].m*parti.hfactor*
-	w0(drmag*parti.invh);
+	kern->w0(drmag*parti.invh);
       parti.invomega += neiblistpart[jj].m*parti.hfactor*
-	parti.invh*womega(drmag*parti.invh);
+	parti.invh*kern->womega(drmag*parti.invh);
 
     }
     // ------------------------------------------------------------------------
