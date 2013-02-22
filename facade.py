ac6009c857d9f79752cf0368888e64603cfd97ac
David Hubber <d.hubber@sheffield.ac.uk>
2013-02-22 18:19:02 +0100

    L1 error norm function in facade.py

--- facade.py
+++ facade.py
@@ -4,7 +4,9 @@ import commandsource as Commands
 from multiprocessing import Manager, Queue
 from plotting import PlottingProcess
 from SimBuffer import SimBuffer, BufferException
+from scipy import interpolate
 import signal
+import numpy
 from time import sleep
 
 try:
@@ -156,6 +158,48 @@ def plotanalytical(x=None, y=None, overplot = True, sim = "current", snap = "cur
     data = command.prepareData()
     Singletons.queue.put([command, data])
 
+
+def L1errornorm(x=None, y=None, xmin=None, xmax=None, sim = "current", snap = "current", autoscale = True):
+    '''Computes the L1 error norm from the simulation data relative to the analytical solution'''
+    
+    #get the simulation number from the buffer
+    if sim == "current":
+        simno = SimBuffer.get_current_sim_no()
+    else:
+        simno = sim
+    
+    #istantiate and setup the command object to retrieve analytical solution
+    command1 = Commands.AnalyticalPlotCommand(x, y, autoscale)
+    command1.sim = simno
+    command1.snap = snap
+    adata = command1.prepareData()
+
+    #istantiate and setup the 2nd command object to retrieve particle data
+    command2 = Commands.ParticlePlotCommand(x, y, autoscale)
+    command2.sim = simno
+    command2.snap = snap
+    pdata = command2.prepareData()
+
+    #cut arrays if limits are provided
+    if xmin != None and xmax != None:
+        aindex = numpy.logical_and( adata.x_data > xmin, adata.x_data < xmax)
+        adata.x_data = adata.x_data[aindex]
+        adata.y_data = adata.y_data[aindex]
+        pindex = numpy.logical_and( pdata.x_data > adata.x_data.min(),
+                                    pdata.x_data < adata.x_data.max() )
+        pdata.x_data = pdata.x_data[pindex]
+        pdata.y_data = pdata.y_data[pindex]
+
+    
+        
+    #prepare interpolation function from analytical data
+    f = interpolate.interp1d(adata.x_data[::-1], adata.y_data[::-1], kind = 'linear', axis=0, bounds_error = False)
+
+    #compute error norm of particle data relative to analytical data
+    L1 = numpy.linalg.norm((pdata.y_data - f(pdata.x_data)), ord=1)/pdata.x_data.size
+    return L1
+
+
 def init():
     global plottingprocess
     plottingprocess = PlottingProcess(Singletons.queue, Singletons.commands)
@@ -182,12 +226,13 @@ if __name__=="__main__":
     loadsim('TEST')
     plot("x","rho")
     plotanalytical("x","rho")
-    limit('x', 0, 10.)
-    snap(99)
+    limit('x', -10.0, 10.0)
+    snap(1)
     import time; time.sleep(2)
     next(); time.sleep(2)
-    snap(7)
+    snap(8)
     limit('x', 'auto')
+    print 'L1 error norm : ',L1errornorm("x","rho",1.0,8.0)
 
     block()
 #    
