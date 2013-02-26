4833bc330d769fc3e9269a4eaaf4fb734913d508
David Hubber <dhubber@usm.lmu.de>
2013-02-26 18:35:22 +0100

    Modifed alltests.py

--- tests/alltests.py
+++ tests/alltests.py
@@ -1,8 +1,25 @@
 from seren.analysis.facade import *
 import os
 import sys
+import subprocess
 
 if __name__ == "__main__":
+    pyfiles = ["adsod-L1error.py","adsod-energyerror.py","soundwave-L1error.py"]
+    for pyfile in pyfiles:
+        try:
+            return_code = subprocess.call(["python",pyfile])
+            print "return_code : ",return_code
+        except:
+            print "Unexpected error while evaluating test script ",pyfile
+            type, value, traceback = sys.exc_info()
+            sys.excepthook(type, value, traceback)
+            print "Going to the next test file..."
+            sys.exc_clear()
+            continue
+
+
+    sys.exit()
+
     #get the files in the same directory of the script
     testfiles = [""]
     for testfile in testfiles:
