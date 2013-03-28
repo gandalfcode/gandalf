1. - Corrected gradient estimates for Godunov SPH 2. - Added flexible approach for different h ranges for different SPH schemes

analysis/analytical.py
@@ -69,8 +69,9 @@ class soundwave (AnalyticalSolution):
         
     def compute(self, ix, iy):
         x = np.arange(self.xL,self.xR,1.0/self.iMAX)
-        print x
         rho = self.rho*(1.0 + self.amp*np.sin(self.kwave*x - self.omega*self.time))
         vx = self.csound*self.amp*np.sin(self.kwave*x - self.omega*self.time)
+        ax = -self.csound*self.csound*self.kwave*self.rho*self.amp*np.cos(self.kwave*x - self.omega*self.time)
         if ix == "x" and iy == "rho": return x,rho
         if ix == "x" and iy == "vx": return x,vx
+        if ix == "x" and iy == "ax": return x,ax