e039de9c298550bd13a5a00c3b3a82370502fbaa
Giovanni Rosotti <rosotti@usm.lmu.de>
2013-05-22 09:24:02 +0200

    Corrected small bug in compute with numpy name

--- analysis/compute.py
+++ analysis/compute.py
@@ -23,10 +23,10 @@ def L1errornorm(x=None, y=None, xmin=None, xmax=None, sim = "current", snap = "c
 
     #cut arrays if limits are provided
     if xmin != None and xmax != None:
-        aindex = numpy.logical_and( adata.x_data > xmin, adata.x_data < xmax)
+        aindex = np.logical_and( adata.x_data > xmin, adata.x_data < xmax)
         adata.x_data = adata.x_data[aindex]
         adata.y_data = adata.y_data[aindex]
-        pindex = numpy.logical_and( pdata.x_data > adata.x_data.min(),
+        pindex = np.logical_and( pdata.x_data > adata.x_data.min(),
                                     pdata.x_data < adata.x_data.max() )
         pdata.x_data = pdata.x_data[pindex]
         pdata.y_data = pdata.y_data[pindex]
@@ -38,7 +38,7 @@ def L1errornorm(x=None, y=None, xmin=None, xmax=None, sim = "current", snap = "c
     f = interpolate.interp1d(adata.x_data[::], adata.y_data[::], kind = 'linear', axis=0, bounds_error = False)
 
     #compute error norm of particle data relative to analytical data
-    L1 = numpy.linalg.norm((pdata.y_data - f(pdata.x_data)), ord=1)/pdata.x_data.size
+    L1 = np.linalg.norm((pdata.y_data - f(pdata.x_data)), ord=1)/pdata.x_data.size
     return L1
 
 
