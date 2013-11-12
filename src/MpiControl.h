f0bec4a0aa291896827f1f53e39d23a9c013b697
Giovanni Rosotti <rosotti@usm.lmu.de>
2013-11-12 16:32:26 +0100

    Added function in MpiControl to receive particles

--- src/MpiControl.h
+++ src/MpiControl.h
@@ -53,6 +53,7 @@ template <int ndim>
 class MpiControl
 {
   void SendParticles(int Node, int Nparticles, int* list);
+  void ReceiveParticles (int Node, int& Nparticles, SphParticle<ndim>* array);
  public:
 
   // Constructor and destructor
