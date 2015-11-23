d913ca76ef8e631a21e97d4f05a5294efb9fae9f
Giovanni Rosotti <rosotti@usm.lmu.de>
2015-11-23 22:54:35 +0000

    Should now have fixed all problems

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,13 +5,13 @@ for file in $(cat to_run)
 do
     if [ -z "$MPICPP" ];
     then
-        mpirun -np 4 ../bin/gandalf $file    
-    else
         ../bin/gandalf $file
+    else
+        mpirun -np 4 ../bin/gandalf $file    
     fi
     if [ $? -ne 0 ];
     then
-        ERRORS += 1
+        ERRORS+=1
     fi
     echo $ERRORS
 done
