9c004b33ec7125c595d1eaf2afa8632fc884233d
Giovanni Rosotti <rosotti@usm.lmu.de>
2015-11-23 11:46:31 +0000

    If compiling with MPI, runs the tests with MPI

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,7 +3,12 @@
 CODE=0
 for file in $(cat to_run)
 do
-    ../bin/gandalf $file
+    if [ -z "$VAR" ];
+    then
+        mpirun -np 4 ../bin/gandalf $file    
+    else
+        ../bin/gandalf $file
+    fi
     CODE=$? || $CODE
     echo $CODE
 done
