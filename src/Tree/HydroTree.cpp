e6bd5487b3159719c3e6a0b00f6415b44b1a3f1e
Giovanni Rosotti <rosotti@ast.cam.ac.uk>
2016-10-03 22:11:56 +0100

    Removed unnecessary message

--- src/Tree/HydroTree.cpp
+++ src/Tree/HydroTree.cpp
@@ -1931,7 +1931,6 @@ void HydroTree<ndim,ParticleType,TreeCell>::UnpackExported
 
 	  if (hydro->Ntot + N_received_part_total > Ntotmax) {
 		  Ntotmax = hydro->Ntot + N_received_part_total;
-		  cout << "Ntotmax: " << Ntotmax << endl;
 		  ReallocateMemory();
 	  }
 
