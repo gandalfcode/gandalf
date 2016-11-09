556109f01eda60be4f0c4acb123378641eca93e9
Giovanni Rosotti <rosotti@ast.cam.ac.uk>
2016-11-09 21:55:46 +0000

    Got rid of useless ghost_range variable

--- src/Hydrodynamics/Ghosts.cpp
+++ src/Hydrodynamics/Ghosts.cpp
@@ -38,10 +38,6 @@
 using namespace std;
 
 
-// Declare ghost_range constant here (prevents warnings with some compilers)
-template <int ndim>
-const FLOAT Ghosts<ndim>::ghost_range = 1.6;
-
 
 //=================================================================================================
 //  Ghosts::CheckBoundaries
