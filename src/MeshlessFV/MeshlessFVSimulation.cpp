d6c28f5c3946f3d872e557622f7bec96e23b9409
Giovanni Rosotti <rosotti@ast.cam.ac.uk>
2016-12-13 12:43:57 +0000

    Fixed same problem in the meshless

--- src/MeshlessFV/MeshlessFVSimulation.cpp
+++ src/MeshlessFV/MeshlessFVSimulation.cpp
@@ -352,6 +352,14 @@ void MeshlessFVSimulation<ndim>::PostInitialConditionsSetup(void)
     }
   }
 
+  // Perform initial MPI decomposition
+  //-----------------------------------------------------------------------------------------------
+#ifdef MPI_PARALLEL
+  mpicontrol->CreateInitialDomainDecomposition(mfv, nbody, simparams, simbox,
+                                               this->initial_h_provided);
+  this->AllocateParticleMemory();
+#endif
+
   // Copy information from the stars to the sinks
   if (simparams->intparams["sink_particles"]==1) {
     sinks->Nsink = nbody->Nstar;
@@ -364,13 +372,6 @@ void MeshlessFVSimulation<ndim>::PostInitialConditionsSetup(void)
     }
   }
 
-  // Perform initial MPI decomposition
-  //-----------------------------------------------------------------------------------------------
-#ifdef MPI_PARALLEL
-  mpicontrol->CreateInitialDomainDecomposition(mfv, nbody, simparams, simbox,
-                                               this->initial_h_provided);
-  this->AllocateParticleMemory();
-#endif
 
   // Set time variables here (for now)
   nresync = 0;   // DAVID : Need to adapt this for block timesteps
