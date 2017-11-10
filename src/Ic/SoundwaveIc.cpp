16766b33d738461f361808d4a5d55dad2cd84f58
David Hubber <david.hubber@googlemail.com>
2017-11-10 12:33:04 +0100

    Changed Ic files to allow soundwave test in 2d and 3d.  Also fixed some small bugs for generating a hexagonal lattice.

--- src/Ic/SoundwaveIc.cpp
+++ src/Ic/SoundwaveIc.cpp
@@ -38,10 +38,6 @@ template <int ndim>
 SoundwaveIc<ndim>::SoundwaveIc(Simulation<ndim>* _sim, FLOAT _invndim) :
   Ic<ndim>(_sim, _invndim)
 {
-  // Some sanity checking to ensure dimensionless units are used
-  if (simparams->intparams["ndim"] != 1) {
-    ExceptionHandler::getIstance().raise("Soundwave sim only runs in 1D");
-  }
   if (simparams->intparams["dimensionless"] == 0) {
     ExceptionHandler::getIstance().raise("dimensionless units required");
   }
@@ -56,94 +52,129 @@ SoundwaveIc<ndim>::SoundwaveIc(Simulation<ndim>* _sim, FLOAT _invndim) :
 template <int ndim>
 void SoundwaveIc<ndim>::Generate(void)
 {
+  int i,k;                          // Particle and dimension counters
+  int Nlattice[ndim];              // Lattice size
+  FLOAT csound;                     // (Isothermal) sound speed
+  FLOAT lambda;                     // Wavelength of perturbation
+  FLOAT kwave;                      // Wave number of perturbing sound wave
+  //FLOAT omegawave;                  // Angular frequency of sound wave
+  FLOAT ugas;                       // Internal energy of gas
+  FLOAT volume;
+  FLOAT *r;                         // Particle positions
+
+  // Make local copies of parameters for setting up problem
+  int Npart       = simparams->intparams["Nhydro"];
+  FLOAT rhofluid1 = simparams->floatparams["rhofluid1"];
+  FLOAT press1    = simparams->floatparams["press1"];
+  FLOAT gamma     = simparams->floatparams["gamma_eos"];
+  FLOAT gammaone  = gamma - (FLOAT) 1.0;
+  FLOAT amp       = simparams->floatparams["amp"];
+  FLOAT temp0     = simparams->floatparams["temp0"];
+  FLOAT mu_bar    = simparams->floatparams["mu_bar"];
+  std::string particle_dist = simparams->stringparams["particle_distribution"];
+  //Nlattice1[0]    = simparams->intparams["Nlattice1[0]"];
+
+  debug2("[SoundwaveIc::Generate]");
+
+  if (hydro->gas_eos == "isothermal") {
+    ugas   = temp0/gammaone/mu_bar;
+    press1 = gammaone*rhofluid1*ugas;
+    csound = sqrt(press1/rhofluid1);
+  }
+  else {
+    ugas   = press1/rhofluid1/gammaone;
+    csound = sqrt(gamma*press1/rhofluid1);
+  }
+
+  lambda = icBox.max[0] - icBox.min[0];
+  kwave = twopi/lambda;
+  //omegawave = twopi*csound/lambda;
+
+  // Allocate local and main particle memory
+  //for (k=0; k<ndim; k++) Nlattice1[k] = 1;
+  Nlattice[0]    = simparams->intparams["Nlattice1[0]"];
+  Nlattice[1]    = simparams->intparams["Nlattice1[1]"];
+  Nlattice[2]    = simparams->intparams["Nlattice1[2]"];
+  //Nlattice1[0] = Npart;
+
   if (ndim == 1) {
+    volume = icBox.max[0] - icBox.min[0];
+    Npart = Nlattice[0];
+  }
+  else if (ndim == 2) {
+    volume = (icBox.max[0] - icBox.min[0])*(icBox.max[1] - icBox.min[1]);
+    Npart = Nlattice[0]*Nlattice[1];
+  }
+  else if (ndim == 3) {
+    volume = (icBox.max[0] - icBox.min[0])*
+      (icBox.max[1] - icBox.min[1])*(icBox.max[2] - icBox.min[2]);
+    Npart = Nlattice[0]*Nlattice[1]*Nlattice[2];
+  }
 
-    int i,k;                          // Particle and dimension counters
-    int Nlattice1[ndim];              // Lattice size
-    FLOAT csound;                     // (Isothermal) sound speed
-    FLOAT lambda;                     // Wavelength of perturbation
-    FLOAT kwave;                      // Wave number of perturbing sound wave
-    //FLOAT omegawave;                  // Angular frequency of sound wave
-    FLOAT ugas;                       // Internal energy of gas
-    FLOAT *r;                         // Particle positions
+  hydro->Nhydro = Npart;
+  bool dusty_wave = simparams->stringparams["dust_forces"] != "none" ;
+  if (dusty_wave) hydro->Nhydro *= 2;
 
-    // Make local copies of parameters for setting up problem
-    int Npart       = simparams->intparams["Nhydro"];
-    FLOAT rhofluid1 = simparams->floatparams["rhofluid1"];
-    FLOAT press1    = simparams->floatparams["press1"];
-    FLOAT gamma     = simparams->floatparams["gamma_eos"];
-    FLOAT gammaone  = gamma - (FLOAT) 1.0;
-    FLOAT amp       = simparams->floatparams["amp"];
-    FLOAT temp0     = simparams->floatparams["temp0"];
-    FLOAT mu_bar    = simparams->floatparams["mu_bar"];
-    //Nlattice1[0]    = simparams->intparams["Nlattice1[0]"];
+  sim->AllocateParticleMemory();
+  r = new FLOAT[ndim*Npart];
 
-    debug2("[SoundwaveIc::Generate]");
+  // Add a cube of random particles defined by the simulation bounding box and
+  // depending on the chosen particle distribution
+  if (particle_dist == "random") {
+    Ic<ndim>::AddRandomBox(Npart, icBox, r, sim->randnumb);
+  }
+  else if (particle_dist == "cubic_lattice") {
+    Ic<ndim>::AddCubicLattice(Npart, Nlattice, icBox, true, r);
+  }
+  else if (particle_dist == "hexagonal_lattice") {
+    Ic<ndim>::AddHexagonalLattice(Npart, Nlattice, icBox, true, r);
+  }
+  else {
+    string message = "Invalid particle distribution option";
+    ExceptionHandler::getIstance().raise(message);
+  }
 
-    if (hydro->gas_eos == "isothermal") {
-      ugas   = temp0/gammaone/mu_bar;
-      press1 = gammaone*rhofluid1*ugas;
-      csound = sqrt(press1/rhofluid1);
+  // Add sinusoidal density perturbation to particle distribution
+  Ic<ndim>::AddSinusoidalDensityPerturbation(Npart, amp, lambda, r);
+
+
+  // Set all other particle quantities
+  //----------------------------------------------------------------------------------------------
+  for (i=0; i<Npart; i++) {
+    Particle<ndim>& part = hydro->GetParticlePointer(i);
+
+    // Set positions in main array with corresponind velocity perturbation
+    for (k=0; k<ndim; k++) part.r[k] = r[ndim*i + k];
+    for (k=0; k<ndim; k++) part.v[k] = 0.0;
+    part.v[0] = csound*amp*sin(kwave*r[ndim*i]);
+    part.m = rhofluid1*volume/(FLOAT) Npart;
+    part.h = hydro->h_fac*pow(part.m/rhofluid1,invndim);
+    part.u = ugas; //*((FLOAT) 1.0 + amp*sin(kwave*r[ndim*i]));
+    part.rho = rhofluid1*(1.0 + amp*sin(twopi*part.r[0]/lambda));
+#ifdef GADGET_SPH
+    part.Aent = gammaone*part.u*pow(part.rho,(FLOAT) 1.0 - gamma);
+#endif
+
+  }
+  //-----------------------------------------------------------------------------------------------
+
+  if (dusty_wave){
+    FLOAT d2g = simparams->floatparams["dust_mass_factor"] ;
+    for (i = 0; i < Npart; ++i) {
+      Particle<ndim>& Pg = hydro->GetParticlePointer(i) ;
+      Particle<ndim>& Pd = hydro->GetParticlePointer(i+Npart) ;
+      Pd = Pg ;
+      Pd.m *= d2g ;
+      Pd.h_dust = Pd.h ;
+      Pd.u = 0 ;
+
+      Pg.ptype = gas_type ;
+      Pd.ptype = dust_type ;
     }
-    else {
-      ugas   = press1/rhofluid1/gammaone;
-      csound = sqrt(gamma*press1/rhofluid1);
-    }
 
-    lambda = icBox.max[0] - icBox.min[0];
-    kwave = twopi/lambda;
-    //omegawave = twopi*csound/lambda;
+    sim->initial_h_provided = true;
+    delete[] r;
 
-    // Allocate local and main particle memory
-    for (k=0; k<ndim; k++) Nlattice1[k] = 1;
-    Nlattice1[0] = Npart;
-
-    hydro->Nhydro = Npart;
-    bool dusty_wave = simparams->stringparams["dust_forces"] != "none" ;
-    if (dusty_wave) hydro->Nhydro *= 2;
-
-    sim->AllocateParticleMemory();
-    r = new FLOAT[ndim*Npart];
-
-    // Add regular distribution of SPH particles
-    Ic<ndim>::AddCubicLattice(Npart, Nlattice1, icBox, false, r);
-
-    // Add sinusoidal density perturbation to particle distribution
-    Ic<ndim>::AddSinusoidalDensityPerturbation(Npart, amp, lambda, r);
-
-    // Set all other particle quantities
-    //----------------------------------------------------------------------------------------------
-    for (i=0; i<Npart; i++) {
-      Particle<ndim>& part = hydro->GetParticlePointer(i);
-
-      // Set positions in main array with corresponind velocity perturbation
-      for (k=0; k<ndim; k++) part.r[k] = r[ndim*i];
-      for (k=0; k<ndim; k++) part.v[k] = csound*amp*sin(kwave*r[ndim*i]);
-      part.m = rhofluid1*lambda/(FLOAT) Npart;
-      part.h = hydro->h_fac*pow(part.m/rhofluid1,invndim);
-      part.u = ugas; //*((FLOAT) 1.0 + amp*sin(kwave*r[ndim*i]));
-
-    }
-    //-----------------------------------------------------------------------------------------------
-
-    if (dusty_wave){
-      FLOAT d2g = simparams->floatparams["dust_mass_factor"] ;
-      for (i = 0; i < Npart; ++i) {
-        Particle<ndim>& Pg = hydro->GetParticlePointer(i) ;
-        Particle<ndim>& Pd = hydro->GetParticlePointer(i+Npart) ;
-        Pd = Pg ;
-        Pd.m *= d2g ;
-        Pd.h_dust = Pd.h ;
-        Pd.u = 0 ;
-
-        Pg.ptype = gas_type ;
-        Pd.ptype = dust_type ;
-      }
-
-      sim->initial_h_provided = true;
-      delete[] r;
-
-    }
   }
 
   return;
